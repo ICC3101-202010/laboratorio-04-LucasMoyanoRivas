005228c9fbc19c4df57f8a34dcb258003d98484a
Lucas Moyano Rivas <lucasmoyanor@Lucass-MacBook-Pro-2.local>
2020-04-11 15:50:30 -0400

    primer commit lab 4

---
+++ CuartoLaboratorio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CuartoLaboratorio
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
